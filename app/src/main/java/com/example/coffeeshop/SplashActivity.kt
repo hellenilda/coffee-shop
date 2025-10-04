51bef9e853795b8c7c669a729ed01473f8876379
hellenilda <hellen95limaaraujo@gmail.com>
2025-10-04 15:15:21 -0300

    :tada: init: SplashScreen

---
+++ app/src/main/java/com/example/coffeeshop/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.coffeeshop
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.coffeeshop.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding= ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
